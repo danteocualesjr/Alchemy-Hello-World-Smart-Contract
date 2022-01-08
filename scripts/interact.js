37683bfeace51e055017cd6598a169205a86b0e1
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-08 15:19:06 +0800

    Created instance of contract

--- scripts/interact.js
+++ scripts/interact.js
@@ -4,3 +4,14 @@ const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 // For Hardhat
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+
+// Created instance of contract
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="ropsten", API_KEY);
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// Contract
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
