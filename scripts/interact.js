c995ab673c0251d1cc5e3ec0fe5fe699cbded18b
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-08 15:17:00 +0800

    Grabbing contract ABI

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,3 +1,6 @@
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+// For Hardhat
+const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
