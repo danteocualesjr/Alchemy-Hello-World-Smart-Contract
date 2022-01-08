361ef09dbf7ef881148fba7386466a9fa7323382
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-08 15:12:32 +0800

    Added provider, signer, and contract

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,3 +1,12 @@
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="ropsten", API_KEY);
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// Contract
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
