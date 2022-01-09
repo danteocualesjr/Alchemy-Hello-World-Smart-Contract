5664ab163116284115ca2ef2c79b9b2290a67671
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-10 01:13:07 +0800

    Added hardhat-etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,10 @@
 
 require('dotenv').config();
 require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-etherscan');
 
 const { API_URL, PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.7.3",
