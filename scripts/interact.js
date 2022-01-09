a4e63cab68f2df0759ab1fed5e4e425004208264
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-09 08:08:38 +0800

    Created function to read the message stored in smart contract and print it to the console

--- scripts/interact.js
+++ scripts/interact.js
@@ -15,3 +15,12 @@ const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 
 // Contract
 const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+// Reading the init message
+
+async function main() {
+    const message = await helloWorldContract.message();
+    console.log('The message is: ' + message);
+}
+
+main();
